887147ba6734a68919dafc676dc9e95d38a5b9fb
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-01 17:03:22 -0700

    car insurance
    car insurance Assignment

---
+++ insurance/insurance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" What is tour age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+            Console.WriteLine("Have you ever had DUI?(True or False)");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.ReadLine();
+            Console.WriteLine(" How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            bool qualification = (age>15 && DUI==false && tickets<3);
+            Console.WriteLine(" qualification? "+ qualification);
+            Console.ReadLine();
+        }
+    }
+}
