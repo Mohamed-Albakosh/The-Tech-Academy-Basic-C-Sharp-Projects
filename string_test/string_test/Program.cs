2302e1c9ae865136823b983294f43759209d7117
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-02 21:03:17 -0700

    Concatenate three strings
    Assignment Concatenate three strings

---
+++ string_test/string_test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str_1 = "Mohamed";
+            string str_2 = "Sleman";
+            string str_3= "Albakosh";
+            string full_name = str_1 +" "+ str_2+" "+str_3 ;
+            Console.WriteLine(full_name.ToUpper());
+            Console.Read();
+
+        }
+    }
+}
