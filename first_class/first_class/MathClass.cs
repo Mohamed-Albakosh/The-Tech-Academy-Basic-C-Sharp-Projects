d0798680a16bc864d32b375c87e5466cec4cb19b
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-08 04:24:58 -0700

    class Assignment
    first class

---
+++ first_class/first_class/MathClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_class
+{
+    class MathClass
+    {
+        public int first_number { get; set; }
+        public int second_number { get; set; }
+        public int third_number { get; set; }
+    }
+}
---
+++ first_class/first_class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass math = new MathClass();
+            Console.Write("enter the first number:");
+            math.first_number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the second number:");
+            math.second_number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the third number:");
+            math.third_number = Convert.ToInt32(Console.ReadLine());
+            int total = math.first_number + math.second_number + math.third_number;
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
