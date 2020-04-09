a1c5ec58a3c1146032e8ae399537176da55a0e9b
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-09 02:42:10 -0700

    Assignment
    last update

---
+++ math_class/math_class/Math_class.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math_class
+{
+    public class Math_class
+    {
+        public Math_class()
+        {
+            Number = 0;
+            Number_2 = 0;
+            name = "";
+            
+        }
+        public int Number { get; set; }
+        public decimal Number_2 { get; set; }
+        public string name { get; set; }
+        public   int AddNumber(int num)
+        {
+            return Number=num + 2;
+        }
+        public  decimal AddNumber(decimal num_2)
+        {
+            return Number_2 = num_2 + 10;
+        }
+        public string AddNumber(string num_3)
+        {
+            return  num_3;
+        }
+
+    }
+}
---
+++ math_class/math_class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("enter int  number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter decimal number:");
+            decimal num_2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("enter the name:");
+            string name = Console.ReadLine();
+
+
+            Math_class math = new Math_class();
+            
+            
+            Console.WriteLine(math.AddNumber(num));
+            Console.WriteLine(math.AddNumber(num_2));
+            Math_class nam = new Math_class();
+            Console.WriteLine(nam.AddNumber(name));
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
