324b5dd9e048fc06058d8a18e2319b30efd31de0
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-10 02:15:21 -0700

    Methodes Assignment
    Assignment page 160

---
+++ Math_Class_2/Math_Class_2/MathClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Class_2
+{
+    class MathClass
+    {
+        public MathClass()
+        {
+            Number = 0;
+           
+
+        }
+        public int Number { get; set; }
+        
+        public int AddNumber(int num_1,int num_2=0)
+        {
+           int  total = 0;
+             total = num_1 + num_2;
+            return total;
+        }
+        
+
+    }
+}
---
+++ Math_Class_2/Math_Class_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Class_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter first number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter second number:");
+            int num_2 = Convert.ToInt32(Console.ReadLine());
+            MathClass math = new MathClass();
+            Console.WriteLine(math.AddNumber(num,num_2));
+            Console.WriteLine("here we pass one number");
+            Console.WriteLine(math.AddNumber(num));
+            Console.ReadLine();
+        }
+    }
+}
