58d0b8cf1bd3e9bd8e30404aab18281f711e8314
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-10 07:26:43 -0700

    inherit Class Assignment
    page 169

---
+++ InheritClass/InheritClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Mohamed";
+            person.LastName = "Albakosh";
+            person.SayName();
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
