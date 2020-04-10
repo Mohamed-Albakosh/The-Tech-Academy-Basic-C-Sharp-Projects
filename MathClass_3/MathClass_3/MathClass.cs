980327e9d14715111cf7a1fe7b7847999466c938
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-10 03:18:44 -0700

    Assignment page 161
    classes Assignment

---
+++ MathClass_3/MathClass_3/MathClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass_3
+{
+   public class MathClass
+    {
+        public MathClass()
+        {
+            Number = 0;
+
+
+        }
+        public int Number { get; set; }
+
+        public  void AddNumber(int FirstNumber, int SeconedNumber)
+        {
+            int total = 0;
+            total = FirstNumber + SeconedNumber;
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ MathClass_3/MathClass_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter first number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter second number:");
+            int num_2 = Convert.ToInt32(Console.ReadLine());
+            MathClass math = new MathClass();
+            math.AddNumber(num,num_2);
+            math.AddNumber(FirstNumber: num, SeconedNumber: num_2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
