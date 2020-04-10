72f9f31f87225e01c08a3346cda23ff7e6460bc3
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-10 05:43:14 -0700

    Static Class
    page 164

---
+++ MathClass_4/MathClass_4/MathClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass_4
+{
+   public static class MathClass
+    {
+       
+        
+
+        public static void AddNumber(int number)
+        {
+             
+            int div= number+2;
+            Console.WriteLine(div);
+        }
+        public static Double AddNumber(double number)
+        {
+
+            double div = number / 2;
+            return div;
+        }
+    }
+}
---
+++ MathClass_4/MathClass_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the  number:");
+            double num_2 = Convert.ToDouble(Console.ReadLine());
+
+            // MathClass math = new MathClass();
+            // math.AddNumber(num);
+            MathClass.AddNumber(num);
+           Console.WriteLine( MathClass.AddNumber(num_2));
+
+
+            Console.ReadLine();
+        }
+    }
+}
