f36defb742d42266590b70eec5eebdfb25b2befc
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-11 02:56:02 -0700

    fix Assignment page 160
    page 160

--- Math_Class_2/Math_Class_2/Program.cs
+++ Math_Class_2/Math_Class_2/Program.cs
@@ -10,15 +10,27 @@ namespace Math_Class_2
     {
         static void Main(string[] args)
         {
-            Console.Write("enter first number:");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Console.Write("enter second number:");
-            int num_2 = Convert.ToInt32(Console.ReadLine());
             MathClass math = new MathClass();
-            Console.WriteLine(math.AddNumber(num,num_2));
-            Console.WriteLine("here we pass one number");
-            Console.WriteLine(math.AddNumber(num));
-            Console.ReadLine();
+            Console.Write("enter how many numbers chose (1 or 2):");
+            int optn = Convert.ToInt32(Console.ReadLine());
+            if (optn == 2)
+            {
+                Console.Write("enter first number:");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.Write("enter second number:");
+                int num_2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(math.AddNumber(num, num_2));
+                Console.ReadLine();
+             }
+            else
+            {
+                Console.Write("enter first number:");
+                int num_3 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("here we pass one number");
+                Console.WriteLine(math.AddNumber(num_3));
+                Console.ReadLine();
+            }
         }
     }
 }
