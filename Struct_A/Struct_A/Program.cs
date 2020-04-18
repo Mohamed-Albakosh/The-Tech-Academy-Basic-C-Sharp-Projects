155ee46e34ec5b9bc3a6f6a904cb93d7549c5216
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-17 17:15:03 -0700

    Struct Assignment
    page 197

---
+++ Struct_A/Struct_A/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            numberStruct num = new numberStruct();
+            num.Amount = 8;
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
