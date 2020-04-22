5c98a862087e71bcc745da4830c61bcd77724a1c
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-22 16:40:46 -0700

    Date and Tie Assignment
    page 221

---
+++ Date_Time/Date_Time/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.Write("What time will be after(Enter a number):");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.AddHours(num));
+            Console.ReadLine();
+        }
+    }
+}
