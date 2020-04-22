617d0efff3c8ce3d1cb2ecca886291b65bde8c97
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-21 21:43:45 -0700

    read and write a file
    page 218

---
+++ Read_write_File/Read_write_File/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Read_write_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Number:");
+            string num=Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Mohamed\Desktop\C#\log.txt", num);
+            int num1 =Convert.ToInt32( File.ReadAllText(@"C:\Users\Mohamed\Desktop\C#\log.txt"));
+            Console.WriteLine(num1);
+            Console.ReadLine();
+        }
+    }
+}
