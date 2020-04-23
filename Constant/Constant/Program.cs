6b1e024b9ca708a2ae66f34dd12bba08b63a3c7c
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-23 01:15:06 -0700

    Constant Assignment
    page 231

---
+++ Constant/Constant/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Mohamed";
+            Console.Write("enter the age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the Country:");
+            var con = Console.ReadLine();
+            conClass c_constant = new conClass(name,age,con);
+            Console.WriteLine(c_constant.Name+" "+c_constant.Age+" "+c_constant.Contry);
+            conClass c_constant_2 = new conClass(name);
+            Console.WriteLine(c_constant_2.Name + " " + c_constant_2.Age + " " + c_constant_2.Contry);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Constant/Constant/conClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant
+{
+    public class conClass
+    {
+        public conClass(string name): this(name,10,"USA")
+        { }
+        public conClass( string name,int ag,string con)
+        {
+            Name = name;
+            Age = ag;
+            Contry = con;
+          
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Contry { get; set; }
+    }
+}
