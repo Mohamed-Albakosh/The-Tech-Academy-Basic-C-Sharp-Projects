5b1d32893cd829b223c1f949a0809a925b16c94e
Mohamed-Albakosh <azzrow@gmail.com>
2020-04-27 00:16:42 -0700

    Exception Assignment
    page 225

---
+++ Aexception/Aexception/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aexception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter your Age:");
+                var age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 0)
+                {
+                    throw new ArgumentException("Age cannot be negative or zreo");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Your Age is:"+age);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
